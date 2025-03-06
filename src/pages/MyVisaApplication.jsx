c6a2d6b788d2c61b16397ec3dfe8d2c0ae1988cf
emonShimoul <emonshimoul@gmail.com>
2025-03-07 03:36:11 +0600

    load individual visa application depending on user

--- src/pages/MyVisaApplication.jsx
+++ src/pages/MyVisaApplication.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
 
 const MyVisaApplication = () => {
+  const { user } = useContext(AuthContext);
+
+  fetch(`http://localhost:5000/visa-application/${user?.email}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <div>
       <h1>My Visa Application</h1>
