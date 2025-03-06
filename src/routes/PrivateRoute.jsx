bdd7eca8eaaac01248d5bfeeabac30c3a1be04fd
emonShimoul <emonshimoul@gmail.com>
2025-03-06 12:55:44 +0600

    added a loading spinner when the data is in a loading state.

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../components/Loading";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
@@ -8,7 +9,7 @@ const PrivateRoute = ({ children }) => {
   const location = useLocation();
 
   if (loading) {
-    return "Loading...";
+    return <Loading></Loading>;
   }
 
   if (user && user?.email) {
